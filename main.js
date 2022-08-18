50c18d2ea66b25fd0d83d87aa1f90a1946898786
TsaiCaro <carotsai@hotmail.es>
2022-08-18 18:44:29 -0300

    primera subida

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+let texto = document.getElementById("displayinput");
+
+
+
+function accion0() {
+    texto.value += "0";
+}
+function accion1() {
+    texto.value += "1";
+}
+function accion2() {
+    texto.value += "2";
+}
+function accion3() {
+    texto.value += "3";
+}
+function accion4() {
+    texto.value += "4";
+}
+function accion5() {
+    texto.value += "5";
+}
+function accion6() {
+    texto.value += "6";
+}
+function accion7() {
+    texto.value += "7";
+}
+function accion8() {
+    texto.value += "8";
+}
+function accion9() {
+    texto.value += "9";
+}
+
+
+function clear() {
+    texto.value += "0";
+}
+console.log(texto);
+// let clear = document.getElementById("clear");
+// clear.onclick = clear;
